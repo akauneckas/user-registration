6d7045f38aaddd442110b41b1731b8354c9b4f0a
Arnoldas Kauneckas <arnoldas.kauneckas@visma.com>
2016-12-05 21:05:02 +0200

    project initialization

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `<div>
+                    
+               </div>`
+})
+export class AppComponent {}
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import 'core-js';
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(success => console.log(`user registration is running..`))
+  .catch(err => console.error(err));
