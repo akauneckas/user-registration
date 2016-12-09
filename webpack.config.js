c0e8e9bf6b2aca798328541494db2c72bb7490cd
Arnoldas Kauneckas <arnoldas.kauneckas@visma.com>
2016-12-09 15:57:04 +0200

    tests setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
+    devtool: 'inline-source-map',
     entry: './src/main.ts',
     output: {
         path: './dist',
